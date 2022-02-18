a2cc2cb72515cc02ef2e81b65b78b065d0487a3c
Maksim <meison98@bk.ru>
2022-02-18 13:41:42 +0300

    Init types

---
+++ video-service/types/movies.types.ts
@@ -0,0 +1,11 @@
+export interface IMovie {
+    name: string;
+	release_year: number;
+	director_first_name: string;
+	director_last_name: string;
+}
+
+export interface IDirector {
+    first_name: string;
+	last_name: string;
+}
